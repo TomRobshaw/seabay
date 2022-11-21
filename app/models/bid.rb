8b7fe762d26e876f411c5e84fdc601b1e1b4fa56
Tom Robshaw <t.robshaw@outlook.com>
2022-11-21 17:27:16 +1100

    controllers complete

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,2 +1,11 @@
 class Bid < ApplicationRecord
+  #after update
+  after_save :create_purchase if :accepted?
+  enum :status, { pending: 0, accepted: 1, declined: 2 }
+
+  private
+
+  def create_purchase
+    Purchase.create(bid: self)
+  end
 end
