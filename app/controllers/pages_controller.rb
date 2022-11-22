b095376d59d23322c78b9ce8837de6950fed08b0
Tom Robshaw <t.robshaw@outlook.com>
2022-11-22 17:00:42 +1100

    add dotenv-rails

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  # skip_before_action :authenticate_user!, only: [ :home ]
+  # make this only allow them to view listings but not bid
   def home
   end
 end
--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,7 +2,7 @@ class Bid < ApplicationRecord
   #after update
   # after_save :create_purchase if :accepted?
   # enum :status, { pending: 0, accepted: 1, declined: 2 }
-
+  belongs_to :listing
   private
 
   def create_purchase
