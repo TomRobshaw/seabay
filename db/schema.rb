7817327293a03c3c6b2d64e76b183d3fabb69f55
Samuel Espinet <samuelespinet30@gmail.com>
2022-11-24 17:04:25 +1000

    updated profile

--- db/schema.rb
+++ db/schema.rb
@@ -49,8 +49,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_004830) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
-    t.index ["listing_id"], name: "index_bid_on_listing_id"
-    t.index ["user_id"], name: "index_bid_on_user_id"
+    t.index ["listing_id"], name: "index_bids_on_listing_id"
+    t.index ["user_id"], name: "index_bids_on_user_id"
   end
 
   create_table "listings", force: :cascade do |t|
@@ -65,10 +65,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_004830) do
 
   create_table "purchases", force: :cascade do |t|
     t.boolean "status", null: false
-    t.bigint "bid_id"
+    t.bigint "bids_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["bid_id"], name: "index_purchase_on_bid_id"
+    t.index ["bids_id"], name: "index_purchases_on_bids_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -89,5 +89,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_004830) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bids", "listings"
   add_foreign_key "bids", "users"
-  add_foreign_key "purchases", "bids"
+  add_foreign_key "purchases", "bids", column: "bids_id"
 end
